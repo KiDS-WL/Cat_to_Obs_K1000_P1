3b296e309452f9736325273beea542e5f4ce2436
Catherine Heymans <heymans@roe.ac.uk>
2019-06-26 16:09:12 +0100

    calc_xi_w_treecorr.py:
    added a commented out hack for the current
    mock incarnation
    we need to change the mocks such that the
    ra and dec columns have the same name
    as the data

--- Calc_2pt_Stats/calc_xi_w_treecorr.py
+++ Calc_2pt_Stats/calc_xi_w_treecorr.py
@@ -33,16 +33,27 @@ cat1 = treecorr.Catalog(fitscat1, ra_col='ALPHA_J2000', dec_col='DELTA_J2000', r
 cat2 = treecorr.Catalog(fitscat2, ra_col='ALPHA_J2000', dec_col='DELTA_J2000', ra_units='deg', dec_units='deg', \
                                   g1_col='e1', g2_col='e2', w_col='weight')
 
+# AN UNFORTUNATE HACK FOR THE MOCKS
+# TBD - make mocks with the same column names as the data and a weight column
+#
+#cat1 = treecorr.Catalog(fitscat1, ra_col='RA', dec_col='DEC', ra_units='deg', dec_units='deg', \
+#                                  g1_col='G1', g2_col='G2')
+#cat2 = treecorr.Catalog(fitscat2, ra_col='RA', dec_col='DEC', ra_units='deg', dec_units='deg', \
+#                                  g1_col='G1', g2_col='G2')
+
+# the value of inbinslop is still TBD
+# when using fine bins I find this is suitable
+inbinslop = 1.5
+# when using broad bins it needs to be much finer
+inbinslop = 0.08
+
 # Define the binning based on command line input
-# Using bin_slop of 0.08 which Linc found to be optimal for xi_+/- on the mocks
 if(lin_not_log=='true'): 
     gg = treecorr.GGCorrelation(min_sep=theta_min, max_sep=theta_max, nbins=nbins, sep_units='arcmin',\
-         bin_type='Linear', bin_slop=0.08)
-else:
-    #gg = treecorr.GGCorrelation(min_sep=theta_min, max_sep=theta_max, nbins=nbins, sep_units='arcmin',\
-    #bin_type='Log', bin_slop=0.08)
+         bin_type='Linear', bin_slop=inbinslop)
+else: # Log is he default bin_type for Treecorr
     gg = treecorr.GGCorrelation(min_sep=theta_min, max_sep=theta_max, nbins=nbins, sep_units='arcmin', \
-         bin_slop=0.08)
+         bin_slop=inbinslop)
 
 # Calculate the 2pt correlation function
 gg.process(cat1,cat2)
