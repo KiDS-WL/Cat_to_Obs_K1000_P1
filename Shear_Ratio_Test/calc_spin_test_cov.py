4323005a4efb8d497d82769690b69bcb218b9fb7
Catherine Heymans <heymans@roe.ac.uk>
2019-10-07 15:00:51 +0100

    calc_spin_test_cov.py:
    Takes the output of the spin test to
    calculate the covariance/correlation matrix

---
+++ Shear_Ratio_Test/calc_spin_test_cov.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+from astropy.io import ascii 
+
+SPINDIR='/disk09/KIDS/K1000_TWO_PT_STATS//OUTSTATS/SHEAR_RATIO/'
+#number of spin trials, angular bins (ntbins) and lens bins (nlbins)
+
+JZBIN=5  # redshift bin of the sources
+ntrials=100
+ntbins=4
+nlbins=5
+
+gtlens=np.zeros([ntbins,ntrials])
+
+for ilens in range(nlbins):
+    for ispin in range(ntrials):
+        gtfile=SPINDIR+'/GT/SPIN/K1000_GT_SPIN_'+str(ispin)+'_6Z_source_'+str(JZBIN)+'_5Z_lens'+str(ilens+1)+'.asc'
+        gtspindat=ascii.read(gtfile)
+        gtlens[:,ispin]=gtspindat['gamT'] 
+    if ilens==0:      
+        gtprev = np.copy(gtlens)
+    else:    
+        gtall = np.vstack((gtprev,gtlens))
+        gtprev= np.copy(gtall)
+
+Cov=np.cov(gtall)
+Corr=np.corrcoef(gtall)
+
+#plt.imshow(Cov, interpolation='None')
+plt.imshow(Corr, interpolation='None')
+plt.colorbar()
+plt.axis('off')
+plt.show()
+plt.savefig('Shear_Ratio_Correlation_source_bin'+str(JZBIN)+'.png')
