d293e7a5a6394c7d713b082be7fb8455a42fb322
benjamingiblin <benjamin.giblin@gmail.com>
2019-12-02 16:31:18 +0000

    Updates to Plot_rho.py in order to calculate the rho-statistics requirements.

--- PSF_systests/Plot_rho.py
+++ PSF_systests/Plot_rho.py
@@ -13,26 +13,29 @@ import time
 import sys
 import os 
 import glob
-rc('text',usetex=True)
-rc('font',size=24)
-rc('legend',**{'fontsize':24})
-rc('font',**{'family':'serif','serif':['Computer Modern']})
 
+
+# NOTE: THE FOLLOWING VARIABLES ARE USED TO SET REQUIREMENTS ON RHO-STATS FOR
+# VISUAL PURPOSES ONLY.
+# ------------------------------------------------ #
 # Set the plotted requirement on the rho stats:
 Requirement = "M18"			# "M18" for Mandelbaum+18: [p_1,2,4<xi_+/SNR, p_2,5<xi_+/(SNR*alpha)
 							# "Z18" for Zuntz+18: xi_+ / 10 
 
-Which_Data = "Data"		# "Data" for K1000 or "Theory" for HaloFit
+Which_Data = "Data"			# "Data" for K1000 or "Theory" for HaloFit
 							# goes into calculation of the requirement bands.
+
+# If using the lowest ZB bin, you might want to smooth the data vector 
+# so the Requirement band is less jaggardy. If so, use this smoothing scale:
+Smooth_Scale = 0.8
+# ------------------------------------------------ #
+
 
 # Define quantities needed for delta_xip
 alpha=0.03 		# worse case scenario PSF leakage is 0.03;
 			   	# but you still need to calculate this term from data
 T_ratio = 1. 	# Will ultimately get this column from Lance's updated Lensfit cats [I think?]
 
-# If using the lowest ZB bin, you might want to smooth the data vector 
-# so the Requirement band is less jaggardy. If so, use this smoothign scale:
-Smooth_Scale = 0.8
 
 
 LFver = ["321"] #["309b",  "319b", "319c", "319d", "321"] # "319",
@@ -105,8 +108,8 @@ for lfv in range(len(LFver)):
 
 
 # To get a rough idea of size of rho stats, read in the xi+- of some data to overplot 
-data_dir = '/disk2/ps1/bengib/KiDS1000_NullTests/Codes_4_My_Eyes/xi_pm/'
-data_ZBlabel = '0.7-0.9'		# '0.1-0.3' 'None'
+data_dir = 'xi_pm_Vectors_4_Overplotting/'  # '/disk2/ps1/bengib/KiDS1000_NullTests/Codes_4_My_Eyes/xi_pm/'
+data_ZBlabel = '0.7-0.9'					# ZBcut on the vector you overplot... '0.7-0.9', '0.1-0.3' '0.1-2.0'
 theta_data, xip_data = np.loadtxt('%s/KAll.BlindA.xi_pm.ZBcut%s.dat' %(data_dir, data_ZBlabel), usecols=(0,1), unpack=True)
 
 # Read in a covariance 
@@ -171,6 +174,10 @@ def Set_Scales(ax):
 	return
 
 
+
+# This function plots all 5 rho stats & \delta\xi_+ in separate panels
+# with symlog y-axes. A yellow band illustrates the Mandelbaum/Zuntz requirements
+# (whichever one is specified at the top)
 def Plot_5_Symlog(rho, rho_err, pm):
 
 	Deltaxip = True
@@ -233,7 +240,10 @@ def Plot_5_Symlog(rho, rho_err, pm):
 	return
 #Plot_5_Symlog(rhop_mean, rhop_err, '+')
 
-def Plot_4Paper(rho, rho_err, pm, lfv):
+# This plots all 5 rho stats on one panel, \delta\xi_+ on a separate panel
+# with a broad band illustrating the Mandelbaum/Zuntz requirements
+# (whichever one is specified at the top).
+def Plot_2Panel(rho, rho_err, pm, lfv):
 
 	fig = plt.figure(figsize = (10,10))
 	gs1 = gridspec.GridSpec(2, 1)
@@ -264,12 +274,13 @@ def Plot_4Paper(rho, rho_err, pm, lfv):
 	plt.savefig('LFver%s/rho1/Plot_Overall-rho%s_CovPatches%sx%s_Require%s_%sxip.png'%(LFver[lfv],pm,Res,Res, Requirement, Which_Data))
 	#plt.show()
 	return
-Plot_4Paper(rhop_mean, rhop_err, '+', -1)
+Plot_2Panel(rhop_mean, rhop_err, '+', -1)
 
 
 
 
-
+# This plots 1 panel showing the fractional size of the \delta\xi_+
+# relative to a noisy theoretical prediction from Marika
 def Plot_xip_Plus_rho(rho, rho_err, pm):
 	fig = plt.figure(figsize = (10,7.5))
 	gs1 = gridspec.GridSpec(1, 1)
@@ -304,14 +315,20 @@ def Plot_xip_Plus_rho(rho, rho_err, pm):
 
 
 t1 = time.time()
-# This function will calculate chi^2 of null hypothesis (xi+ is just noise)...
-# ...the chi^2 of alternative - that xi+ is infected by delta-xi+, and do ...
-# ... P- value analysis to see if those hypotheses are statistically distinguishable.
+# This function will calculate chi^2 values for many noise realisations under 4 hypotheses:
+# null : the chi^2 values obtained for the correct cosmology given no systematics
+# sys: the chi^2 values obtained for the correct cosmology given the systematic bias \delta\xi_+
+# hi/lo: the chi^2 values obtained for a cosmology that is ~0.004 higher in S_8 than the truth.
+# This funcion evaluates if the shift (null-->sys) is subdominant to the shift (null-->hi/lo)
+# Turns out it is, therefor systematic is subdominant to this tiny change in cosmology.
+
 def Investigate_chi2(rho):
 	Cov_Mat_uc_Survey_All = np.loadtxt('%s/Raw_Cov_Mat_Values.dat' %Cov_inDIR)[0:135,0:135] * Linc_Rescale
+	# [0:135,0:135] pulls out only the xi+ elements.
 	lfv = 0
 	delta_xip = T_ratio**2*(rho[lfv,0,:]+rho[lfv,2,:]+rho[lfv,3,:]) - T_ratio*alpha*(rho[lfv,1,:]+rho[lfv,4,:])
 	# append to itself 15 times for the 15 bins (delta_xip same for every bin).
+	# NB: THIS NEEDS TO BE CHANGED, TO HAVE ALPHA VALUES AND THUS DELTA_XIP SPECIFIC TO EACH BIN.
 	delta_xip_stack = np.tile(delta_xip, 15)
 
 	# Assemble the theory vector - used to guage signif. of measuring genuine signal.
@@ -355,20 +372,62 @@ def Investigate_chi2(rho):
 								np.dot( np.linalg.inv(Cov_Mat_uc_Survey_All), noise+xip_theory_stack_lo-xip_theory_stack_fid ))
 
 	# Histogram the chi^2
-	histo_chi2, tmp_bins = np.histogram(chi2_null, 100)
-	bins_chi2 = tmp_bins[:-1] + (tmp_bins[1] - tmp_bins[0])/2.			# get bin centres
-	histo_chi2 = histo_chi2 / simps( histo_chi2, bins_chi2)				# Normalise to a PDF
+	def Hist_n_Normalise(chi2, nbins):
+		histo_chi2, tmp_bins = np.histogram(chi2, nbins)
+		bins_chi2 = tmp_bins[:-1] + (tmp_bins[1] - tmp_bins[0])/2.			# get bin centres
+		histo_chi2 = histo_chi2 / simps( histo_chi2, bins_chi2)				# Normalise to a PDF
+		mean_chi2 = np.mean( chi2 )
+		return histo_chi2, bins_chi2, mean_chi2
+	histo_chi2_null, bins_chi2_null, mean_chi2_null = Hist_n_Normalise(chi2_null, 100)
+	histo_chi2_sys, bins_chi2_sys, mean_chi2_sys = Hist_n_Normalise(chi2_sys, 100)
+	histo_chi2_hi, bins_chi2_hi, mean_chi2_hi = Hist_n_Normalise(chi2_hi, 100)
+	histo_chi2_lo, bins_chi2_lo, mean_chi2_lo = Hist_n_Normalise(chi2_lo, 100)
+	print("Shift in the mean of the chi2 distributions between null and the following hypotheses is...:" )
+	print("sys: ", abs(mean_chi2_null-mean_chi2_sys))
+	print("high S8: ", abs(mean_chi2_null-mean_chi2_hi))
+	print("low S8: ", abs(mean_chi2_null-mean_chi2_lo))
+	
+
+
+	# Plot the chi^2 distribution
+	f, ((ax1)) = plt.subplots(1, 1, figsize=(10,9))
+	ax1.plot(bins_chi2_hi, histo_chi2_hi, color='magenta', linewidth=3)
+	ax1.plot(bins_chi2_sys, histo_chi2_sys, color='orange', linewidth=3)
+	ax1.plot(bins_chi2_null, histo_chi2_null, color='cyan', linewidth=3)
+
+	#ax1.plot( chi2_array, pdf_bf, color='blue', linestyle=':', linewidth=3, label=r'$\rm{DOF}=%.1f \pm %.1f$'%(dof_bf,np.sqrt(dof_err)) )
+
+	# Mark the mean of the chi2 distributions to see how distinguishable they are.
+	ax1.plot( [mean_chi2_hi,mean_chi2_hi], [0., histo_chi2_null.max()], 
+				color='magenta', linestyle='--', linewidth=3, label=r'$\langle \chi^2_{\Delta S_8} \rangle$' )
+	ax1.plot( [mean_chi2_sys,mean_chi2_sys], [0., histo_chi2_null.max()], 
+				color='orange', linestyle='--', linewidth=3, label=r'$\langle \chi^2_{\rm sys} \rangle$' )
+	ax1.plot( [mean_chi2_null,mean_chi2_null], [0., histo_chi2_null.max()], 
+				color='cyan', linestyle='--', linewidth=3, label=r'$\langle \chi^2_{\rm null} \rangle$' )
+
+	#ax1.set_xlim([ chi2_null.min(), chi2_null.max() ])
+	ax1.set_xlabel(r'$\chi^2$')
+	ax1.set_ylabel(r'PDF$(\chi^2)$')
+	ax1.legend(loc='best', frameon=True)
+	#plt.savefig('LFver%s/rho1/Plot_PDF-chi2.png'%(LFver[0]))
+	plt.show()
+
+
+	# The following bits of code are no longer used - they compare the chi^2 distributions
+	# via a P-value analysis. Instead of doing this, we now just look at the means of the chi^2 distributions
+	# (done above). 
+	# -------------------------------------------------------------------------------------------- #
 
 	# Fit for the effective DoF of the chi^2 distribution
 	def chi2pdf_model(chi2_array, *dof):
 		chi2_pdf = chi2.pdf( chi2_array, dof ) / simps( chi2.pdf( chi2_array, dof ), chi2_array)
 		# Interpolate the chi^2 to bins of histogram.
-		return np.interp( bins_chi2, chi2_array, chi2_pdf )	
+		return np.interp( bins_chi2_null, chi2_array, chi2_pdf )	
 
 	chi2_array = np.linspace( 0., 400., 1000 )		# Arbitrary array spanning a suitable range of chi2 values
 	p0 = [100]										# Arbitrary first guess at the DoF
 	dof_bf, dof_err = curve_fit(chi2pdf_model, 
-					chi2_array, histo_chi2, p0=p0)	# Best fit dof.
+					chi2_array, histo_chi2_null, p0=p0)	# Best fit dof.
 	pdf_bf = chi2.pdf( chi2_array, dof_bf ) / simps( chi2.pdf( chi2_array, dof_bf ), chi2_array)
 
 
@@ -378,26 +437,6 @@ def Investigate_chi2(rho):
 	P_hi = chi2.cdf( chi2_hi, dof_bf )	
 	P_lo = chi2.cdf( chi2_lo, dof_bf )	
 
-
-	# Plot the chi^2 distribution
-	f, ((ax1)) = plt.subplots(1, 1, figsize=(10,9))
-	ax1.bar(bins_chi2, histo_chi2, width=(bins_chi2[1]-bins_chi2[0]), color='red', edgecolor='red', alpha=1.)
-	ax1.plot( chi2_array, pdf_bf, color='blue', linestyle=':', linewidth=3, label=r'$\rm{DOF}=%.1f \pm %.1f$'%(dof_bf,np.sqrt(dof_err)) )
-
-	noise_idx = 0
-	# Mark the chi^2 of the 3 hypothese for a given noise realisation - 
-	# further apart these lines, the more distinguishable they are. 
-	ax1.plot( [chi2_null[noise_idx],chi2_null[noise_idx]], [0., histo_chi2.max()], 
-				color='black', linestyle='-', linewidth=3, label=r'$\chi^2_{{\rm null},%s}$'%noise_idx )
-	ax1.plot( [chi2_sys[noise_idx],chi2_sys[noise_idx]], [0., histo_chi2.max()], 
-				color='black', linestyle='--', linewidth=3, label=r'$\chi^2_{{\rm sys},%s}$'%noise_idx )
-
-	ax1.set_xlim([ chi2_null.min(), chi2_null.max() ])
-	ax1.set_xlabel(r'$\chi^2$')
-	ax1.set_ylabel(r'PDF$(\chi^2)$')
-	ax1.legend(loc='best', frameon=True)
-	plt.savefig('LFver%s/rho1/Plot_PDF-chi2.png'%(LFver[0]))
-	#plt.show()
 
 	def Plot_Pvalue_Histo(P, label):
 		# Plot the distribution of P-values for the noise-realisations
@@ -425,10 +464,13 @@ def Investigate_chi2(rho):
 		#plt.show()
 		return histo_P, cumul_P
 	P_stack = np.vstack(( P_hi, P_lo, P_sys, P_null ))
-	#histo_P_stack = P_stack = np.vstack(( P_hi-P_null, P_lo-P_null, P_sys-P_null ))
 	histo_P_stack, cumul_P_stack = Plot_Pvalue_Histo(P_stack, [r'high $S_8$', r'low $S_8$', 'sys', 'null'] )
 
-	# Do KS tests between PDFs of hi/lo/null and systematic hypotheses
+	# Do KS tests between the P-values of the chi^2's of the hi/lo/null and systematic hypotheses
+	# Could compare the PDFs or the CDFs. Turns out it makes a big difference
+	# with PDFs varying wildly with the noise realisations, and the CDFs 
+	# being completely indistinguishable due to the ~flatness of the PDFs.
+	# This is part of the reason we no longer do this P-value analysis.
 	# "-------------------PDF KS Values---------------------" 
 	KS_sys_null = ks_2samp(histo_P_stack[2,:], histo_P_stack[3,:])
 	KS_hi_null = ks_2samp(histo_P_stack[0,:], histo_P_stack[3,:])
@@ -438,6 +480,8 @@ def Investigate_chi2(rho):
 	#KS_hi_null = ks_2samp(cumul_P_stack[0,:], cumul_P_stack[3,:])
 	#KS_lo_null = ks_2samp(cumul_P_stack[1,:], cumul_P_stack[3,:])
 	# "------------------------------------------------------"
+
+	# -------------------------------------------------------------------------------------------- #
 
 
 	return histo_P_stack, KS_sys_null, KS_hi_null, KS_lo_null
