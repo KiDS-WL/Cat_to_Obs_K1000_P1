Changed the plotting notation and fixed the weights to be LF weights in calculating the weighted average of the T quantities. Have spotted why deltaTPSF/Tgal changes sign with z bin but not yet fixed it.

PSF_systests/Functions_4_Plotting_PSFstats.py
@@ -136,8 +136,8 @@ def Calc_Important_Tquantities(LFver,zbounds, nboot):
         T_g = np.append(T_Ng[idx_N], T_Sg[idx_S])
         dT_p = np.append(delta_TPSF_Ng, delta_TPSF_Sg)
         weight_g = np.append(weight_Ng[idx_N], weight_Sg[idx_S])
-        weight_q1 = (T_g**2 * weight_g ) / dT_p #weight_g #np.ones_like(T_g) #(T_g**2 * weight_g ) / dT_p
-        weight_q1 = np.nan_to_num( weight_q1, nan=0., posinf=0. ) # get rid of nan/inf weights from where dT_p=0
+        weight_q1 = weight_g #(T_g**2 * weight_g ) / dT_p
+        #weight_q1 = np.nan_to_num( weight_q1, nan=0., posinf=0. ) # get rid of nan/inf weights from where dT_p=0
         # ^ that weight computed with error propagation:
         # if y=a/x, sigma_y^2=(-a/x^2)sigma_x^2, sigma_(x/y)^2=1/weight_(x/y)
         # then x=T_g, a=dT_p, y=dT_p/T_g
@@ -148,7 +148,7 @@ def Calc_Important_Tquantities(LFver,zbounds, nboot):
         deltaT_ratio[1,i] = Bootstrap_Error(nboot, dT_p/T_g, weights=weight_q1)
 
         # 2. < T_gal^-2 > & SIGMA[ T_gal^-2  ]
-        weight_q2 = T_g**2 * weight_g #weight_g #np.ones_like(T_g) #T_g**2 * weight_g
+        weight_q2 = weight_g  #weight_g #T_g**2 * weight_g
         # ^ computed with error prop: y=1/x, sigma_y^2=(dy/dx)sigma_x^2, sigma_x^2=1/weight_x
         # with x=T_g, weight_x=weight_g
         Tg_inv = np.average( 1./T_g, weights=weight_q2 )

PSF_systests/Plot_PHterms.py
@@ -34,6 +34,7 @@ font = {'family' : 'serif',
         'size'   : 14} # 19
 
 plt.rc('font', **font)
+plt.rcParams["mathtext.fontset"] = "cm"
 
 LFver = ["321"] #["309b",  "319b", "319c", "319d", "321"] # "319",
 
@@ -52,7 +53,7 @@ Read_Tvalues = True # If True, read in pre-saved values of gal & PSF size quanti
 if Read_Tvalues:
         print('Reading in pre-saved T-quantities for each ZB bin.')
         for lfv in range(len(LFver)):
-                deltaT_ratio_tot[lfv,0,:], deltaT_ratio_tot[lfv,1,:],Tg_invsq_tot[lfv,0,:], Tg_invsq_tot[lfv,1,:] = np.loadtxt('LFver%s/PHterms/deltaTratio_TgInvSq_MeansAndErrors_%stomobins_wgweights.dat' %(LFver[lfv],num_zbins_tot),usecols=(0,1,2,3), unpack=True)
+                deltaT_ratio_tot[lfv,0,:], deltaT_ratio_tot[lfv,1,:],Tg_invsq_tot[lfv,0,:], Tg_invsq_tot[lfv,1,:] = np.loadtxt('LFver%s/PHterms/deltaTratio_TgInvSq_MeansAndErrors_%stomobins.dat' %(LFver[lfv],num_zbins_tot),usecols=(0,1,2,3), unpack=True)
                 
 else:
         # Calculate them
@@ -167,18 +168,6 @@ def Set_Heymans_Constraints(rho):
 	return delta_chi2
 
 
-# Set the requirements on rho_1,2,4 and rho_2,5
-Requirement = None
-if Requirement == "M18":
-	Req_134, Req_25 = Set_Mandelbaum_Constraints(alpha.max())  # Just set single alpha val to 0.03 for now.
-                                						# need to edit Mandelbaum_* func to work with alpha per bin
-elif Requirement == "Z18":
-	Req_134 = gaussian_filter( xip_data, Smooth_Scale ) / 10.
-	Req_25  = gaussian_filter( xip_data, Smooth_Scale ) / 10.
-else:
-	print("Requirement must be set to M18 or Z18")
-	#sys.exit()
-
 
 def Set_Scales(ax):
 	ax.set_xscale('log')
@@ -186,120 +175,6 @@ def Set_Scales(ax):
 	#ax.set_yscale('log')
 	return
 
-
-
-# This function plots all 5 rho stats & \delta\xi_+ in separate panels
-# with symlog y-axes. A yellow band illustrates the Mandelbaum/Zuntz requirements
-# (whichever one is specified at the top)
-def Plot_5_Symlog(rho, rho_err, pm):
-	
-	#delta_xip, err_delta_xip = Calc_delta_xip_J16( alpha, T_ratio, rho, rho_err )
-	#delta_xip, err_delta_xip, _ = Calc_delta_xip_H20( T_ratio, rho, rho_err )
-	delta_xip, err_delta_xip = Calc_delta_xip_cterms( )
-
-	Deltaxip = True
-	if Deltaxip:
-		scrollthrough=6
-	else:
-		scrollthrough=5
-
-	fig = plt.figure(figsize = (24,10))
-	gs1 = gridspec.GridSpec(3, 2)
-	colors = [ 'magenta', 'darkblue', 'dimgrey', 'orange', 'lawngreen', 'cyan' ] 	
-	for i in range(scrollthrough):
-			ax = plt.subplot(gs1[i], adjustable='box')
-			Set_Scales(ax)
-
-			# Set the requirement bands - rho_2,5 have different requirements than rho_1,3,4
-			if i==1 or i==4:
-				# rho 2 and 5 have one requirement
-				symlogscale = 1e-6
-				ax.fill_between(theta_data[:], y1=abs(Req_25)*-1, y2=abs(Req_25)*1, facecolor='yellow') 
-				ax.set_ylim([ abs(Req_25[0])*-1, abs(Req_25[0])*1 ])
-			elif i==0 or i==2 or i==3:
-				symlogscale = 1e-8
-				# rho 1,3 and 4 have another requirement
-				ax.fill_between(theta_data[:], y1=abs(Req_134)*-1, y2=abs(Req_134)*1, facecolor='yellow') 
-				ax.set_ylim([ abs(Req_134[0])*-1, abs(Req_134[0])*1 ])
-			else:
-				symlogscale = 1e-7
-				Req = np.sqrt( np.diagonal(Cov_Mat_uc_Survey) ) / 2.
-				ax.fill_between(theta_data[:], y1=abs(Req)*-1, y2=abs(Req)*1, facecolor='yellow') 
-				
-				
-			ax.set_yscale('symlog', linthreshy=symlogscale )
-			ax.plot( [0.5,300.], [symlogscale, symlogscale],  'k--' )
-			ax.plot( [0.5,300.], [-1*symlogscale, -1*symlogscale], 'k--' )
-
-			# Set the theta bands
-			if i ==5:
-				ax.set_ylabel(r'$\delta\xi_%s(\theta)$'%pm)
-				ax.set_xlabel(r'$\theta$ [arcmin]')
-				for lfv in range(len(LFver)):
-					idx_max_alpha = np.argmax(alpha[lfv])
-					ax.errorbar(theta, delta_xip[lfv,idx_max_alpha,:], yerr=err_delta_xip[lfv,idx_max_alpha,:], 
-													color=colors[lfv], linewidth=3, label=r'%s'%Plot_Labels[lfv])
-			else:
-				ax.set_ylabel(r'$\rho_{%s}(\theta)$'%(i+1))
-				if i!=3 and i!=4:
-					ax.set_xticks([])
-				else:
-					ax.set_xlabel(r'$\theta$ [arcmin]')
-					# 1, 3, 4 have another requirement
-				for lfv in range(len(LFver)):
-					ax.errorbar(theta, rho[lfv,i,:], yerr=rho_err[lfv,i,:], color=colors[lfv], linewidth=3, label=r'%s'%Plot_Labels[lfv])
-
-	#fig.suptitle('Lensfit v'+LFver[0])
-	if Deltaxip == False:
-		plt.subplot(gs1[-1], adjustable='box').set_visible(False)
-	plt.subplots_adjust(hspace=0)
-	plt.savefig('LFver%s/rho1/Plot_rho%s_CovPatches%sx%s_Require%s_%sxip_Symlog.png'%(LFver[0],pm,Res,Res,Requirement,Which_Data))
-	plt.show()
-	return
-#Plot_5_Symlog(rhop_mean, rhop_err, '+')
-
-# This plots all 5 rho stats on one panel, \delta\xi_+ on a separate panel
-# with a broad band illustrating the Mandelbaum/Zuntz requirements
-# (whichever one is specified at the top).
-def Plot_2Panel(rho, rho_err, pm, lfv):
-
-	#delta_xip, err_delta_xip = Calc_delta_xip_J16( alpha, T_ratio, rho, rho_err )
-	delta_xip, err_delta_xip = Calc_delta_xip_cterms( )
-
-	# Just extract the correct LensFit version
-	delta_xip = delta_xip[lfv]
-	err_delta_xip = err_delta_xip[lfv]
-
-	fig = plt.figure(figsize = (10,10))
-	gs1 = gridspec.GridSpec(2, 1)
-	colors = [ 'magenta', 'darkblue', 'red', 'orange', 'lawngreen', 'cyan' ] 	
-	for i in range(2):
-		ax = plt.subplot(gs1[i], adjustable='box')
-		Set_Scales(ax)
-		ax.set_yscale('log')
-
-		ax.fill_between(theta_data[:], y1=-1*abs(Req_25), y2=abs(Req_25), facecolor='pink') 
-		ax.fill_between(theta_data[:], y1=-1*abs(Req_134), y2=abs(Req_134), facecolor='yellow') 
-		if i == 0:
-			ax.set_ylim( [1.1e-10,1e-4] )
-			ax.set_ylabel(r'$|\rho(\theta)|$')
-			ax.set_xticks([])
-			for j in range(5):
-				ax.errorbar(theta, abs(rho[lfv,j,:]), yerr=abs(rho_err[lfv,j,:]), color=colors[j], linewidth=3, label=r'$\rho_%s$'%(j+1) )	
-		ax.legend(loc='upper right', frameon=False, ncol=3)
-		if i == 1:
-			ax.set_ylim( [1.1e-10,1e-4] )
-			ax.set_ylabel(r'$\delta\xi_%s(\theta)$'%pm)
-			for j in range(num_zbins_tot):
-				ax.errorbar(theta, abs(delta_xip[j,:]), yerr=abs(err_delta_xip[j,:]), linewidth=3)
-			ax.set_xlabel(r'$\theta$ [arcmin]')
-
-	plt.subplots_adjust(hspace=0, wspace=0)
-	plt.savefig('LFver%s/rho1/Plot_Overall-rho%s_CovPatches%sx%s_Require%s_%sxip.png'%(LFver[lfv],pm,Res,Res, Requirement, Which_Data))
-	#plt.show()
-	return
-
-#Plot_2Panel(rhop_mean, rhop_err, '+', -1)
 
 
 # This plots the various ingredients of 
@@ -322,7 +197,7 @@ def Plot_deltaxips_Only():
 	ax.set_yscale('symlog', linthreshy=symlogscale )
 	ax.plot( [0.5,300.], [symlogscale, symlogscale],  'k-' )
 	ax.plot( [0.5,300.], [-1*symlogscale, -1*symlogscale], 'k-' )
-	ax.set_ylim([-1*1e-5, 1e-3])
+	ax.set_ylim([-1*1e-5, 1e-1])
 
 
 	# Plot the diagonal covariance
@@ -335,17 +210,17 @@ def Plot_deltaxips_Only():
 		 label=r'$ 2 \left[{\frac{\overline{\delta T_{\rm PSF}}}{T_{\rm gal}}}\right] \left< e_{\rm obs}^{\rm perfect} e_{\rm obs}^{\rm perfect} \right>$' )
 	ax.errorbar( theta, delta_xip_terms_H[lfv,zbin,:,1], yerr=err_delta_xip_terms_H[lfv,zbin,:,1],
                  color='blue', linewidth=2, linestyle='--',
-		 label=r'$\overline{T_{\rm gal}^{-2}} \left< (\delta e_{\rm PSF} \, T_{\rm PSF}) \,  (\delta e_{\rm PSF} \,T_{\rm PSF}) \right>$' )
+		 label=r'$\left\langle \frac{1}{T_{\rm gal}} \right\rangle^2 \left< (\delta e_{\rm PSF} \, T_{\rm PSF}) \,  (\delta e_{\rm PSF} \,T_{\rm PSF}) \right>$' )
 	ax.errorbar( theta, delta_xip_terms_H[lfv,zbin,:,2], yerr=err_delta_xip_terms_H[lfv,zbin,:,2],
                  color='blue', linewidth=2, linestyle='-.',
-		 label=r'$\overline{T_{\rm gal}^{-2}}\left< (e_{\rm PSF} \, \delta T_{\rm PSF}) \,  (e_{\rm PSF} \, \delta T_{\rm PSF}) \right>$' )
+		 label=r'$\left\langle \frac{1}{T_{\rm gal}} \right\rangle^2 \left< (e_{\rm PSF} \, \delta T_{\rm PSF}) \,  (e_{\rm PSF} \, \delta T_{\rm PSF}) \right>$' )
 	ax.errorbar( theta, delta_xip_terms_H[lfv,zbin,:,3], yerr=err_delta_xip_terms_H[lfv,zbin,:,3],
                  color='blue', linewidth=2, linestyle='-',
-		 label=r'$2 \overline{T_{\rm gal}^{-2}} \left< (e_{\rm PSF} \, \delta T_{\rm PSF}) \,  (\delta e_{\rm PSF} \, T_{\rm PSF}) \right>$' )
+		 label=r'$2 \left\langle \frac{1}{T_{\rm gal}} \right\rangle^2 \left< (e_{\rm PSF} \, \delta T_{\rm PSF}) \,  (\delta e_{\rm PSF} \, T_{\rm PSF}) \right>$' )
 
 	ax.errorbar( theta, delta_xip_H[lfv,zbin,:], yerr=err_delta_xip_H[lfv,zbin,:], color='red',
                      linewidth=2, label=r'$\delta\xi_+^{\rm sys}$' ) 
-	ax.plot( theta, delta_xip_c[lfv,zbin,:], color='dimgrey', linewidth=2, label=r'$\langle cc \rangle$' )
+	ax.plot( theta, delta_xip_c[lfv,zbin,:], color='dimgrey', linewidth=2, label=r'$\langle \delta\epsilon^{\rm PSF}(x,y) \, \delta\epsilon^{\rm PSF}(x,y) \rangle$' )
 
 	ax.legend(loc='upper right', frameon=False, ncol=2)
 	plt.subplots_adjust(hspace=0)
@@ -353,41 +228,6 @@ def Plot_deltaxips_Only():
 	plt.show()
 	return
 Plot_deltaxips_Only()
-
-
-# This plots 1 panel showing the fractional size of the \delta\xi_+
-# relative to a noisy theoretical prediction from Marika
-def Plot_xip_Plus_rho(rho, rho_err, pm):
-	
-	fig = plt.figure(figsize = (10,7.5))
-	gs1 = gridspec.GridSpec(1, 1)
-	# Read in a noisy mock data vector from Marika
-	xip_Mock = np.loadtxt('Marika_Noisy_Data_Mock/xipm_nBins1_0.50-300.00_real0')[:7] # Only first 7 points are for xi+
-
-	colors = [ 'magenta', 'darkblue', 'dimgrey', 'orange', 'lawngreen', 'cyan' ] 	
-
-	ax = plt.subplot(gs1[0], adjustable='box')
-	Set_Scales(ax)
-	ax.set_ylabel(r'$(\xi_+ + \delta_{\xi_+})/\xi_+)$')
-
-	ax.set_xlabel(r'$\theta$ [arcmin]')
-	
-	for lfv in range(len(LFver)):
-		# This delta_xip is eqn 3.9 in Zuntz.
-		delta_xip = T_ratio**2*(rho[lfv,0,:7]+rho[lfv,2,:7]+rho[lfv,3,:7]) - T_ratio*alpha*(rho[lfv,1,:7]+rho[lfv,4,:7])
-		ax.errorbar(theta[:7], (xip_Mock+delta_xip)/xip_Mock, yerr=rho_err[lfv,0,:7]/xip_Mock, color=colors[lfv], linewidth=3, label=r'%s'%Plot_Labels[lfv])
-	ax.plot([0.001,1000], [1,1], 'k--')
-
-	ax.legend(loc='lower right', frameon=False)
-	plt.subplots_adjust(hspace=0)
-	plt.title(r'%s PSF leakage'%alpha)
-	plt.savefig('LFver%s/rho1/Plot_deltarho%s_CovPatches%sx%s.png'%(LFver[0],pm,Res,Res))
-	#plt.show()
-
-	return
-#Plot_xip_Plus_rho(rhop_mean, rhop_err, '+')
-
-
 
 
 
@@ -404,8 +244,8 @@ def Investigate_chi2(rho):
 	# [0:135,0:135] pulls out only the xi+ elements.
 	lfv = 0
 	#delta_xip, _ = Calc_delta_xip_J16( alpha, T_ratio, rho, rho )
-	#delta_xip, _ = Calc_delta_xip_H20( T_ratio, rho, rho )
-	delta_xip, err_delta_xip = Calc_delta_xip_cterms( )
+	delta_xip,_,_,_ = Calc_delta_xip_H20( rho, rho )
+	#delta_xip, err_delta_xip = Calc_delta_xip_cterms( )
 
 	delta_xip = np.ndarray.flatten( delta_xip[lfv] )
 
@@ -550,7 +390,7 @@ def Investigate_chi2(rho):
 
 
 	return histo_P_stack, KS_sys_null, KS_hi_null, KS_lo_null
-#histo_P_stack, KS_sys_null, KS_hi_null, KS_lo_null = Investigate_chi2(rhop_mean)
+#histo_P_stack, KS_sys_null, KS_hi_null, KS_lo_null = Investigate_chi2(php_mean)
 t2 = time.time()
 print(" It took %.0f seconds." %(t2-t1))
 