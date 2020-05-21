fe49cb29fe6e74ac2dc3d14af0d5aa2113dff0f7
benjamingiblin <benjamin.giblin@gmail.com>
2020-05-21 16:56:09 +0100

    Edited the shear ratio test code such that you can now run it with each source+lens bin combination individually, or with all bins at once (as we used to do) just by toggling a variable.

--- Shear_Ratio_Test/Compare_MagOnOff.py
+++ Shear_Ratio_Test/Compare_MagOnOff.py
@@ -1,6 +1,10 @@
 # 07/04/2020, B. M. Giblin, Postdoc, Edinburgh
-# Read in and compare the model fits which include and exclude
-# the impact of mgnification
+# Read in and compare the model fits:
+
+Compare_Mag = False         # Compare fit params/model including/excluding
+                           # the impact of magnification.
+Compare_Single_Bin = True  # Compare fit params obtained for each l+s bin individually
+                           # across lens bins (should be consistent)
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,20 +32,39 @@ if "MICE2" in SOURCE_TYPE:
 
 OUTDIR = INDIR + '/' + Cov_Method.upper()
 
-params_m = np.loadtxt(OUTDIR+'/%sx%s_FitParams_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,True))
-params_nm = np.loadtxt(OUTDIR+'/%sx%s_FitParams_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,False))
-thetas, model_m = np.loadtxt(OUTDIR+'/%sx%s_FitModel_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,True),
-                           usecols=(0,1), unpack=True)
-thetas, model_nm = np.loadtxt(OUTDIR+'/%sx%s_FitModel_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,False),
-                           usecols=(0,1), unpack=True)
-
 nz_source = 5
 nz_lens = 5
 nz_bins = nz_source*nz_lens
 ntheta = 4
 
-# Plot the model
+
+if Compare_Mag:
+    # Compare the fit params and model for Mag On with Mag Off
+    params_m = np.loadtxt(OUTDIR+'/%sx%s_FitParams_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,True))
+    params_nm = np.loadtxt(OUTDIR+'/%sx%s_FitParams_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,False))
+    thetas, model_m = np.loadtxt(OUTDIR+'/%sx%s_FitModel_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,True),
+                                 usecols=(0,1), unpack=True)
+    thetas, model_nm = np.loadtxt(OUTDIR+'/%sx%s_FitModel_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,False),
+                                  usecols=(0,1), unpack=True)
+
+
+    
+if Compare_Single_Bin:
+    # Compare the fit params obtained for each l+s bin, across lens bins
+    params_sl = np.zeros([nz_source, nz_lens])
+    params_sl_err = np.zeros([nz_source, nz_lens])
+    for i in range(nz_source):
+        for j in range(nz_lens):
+            params_sl[i,j] = np.loadtxt(OUTDIR+'/%sx%s_FitParams_MagFalse_t%ss%s.dat'%(SOURCE_TYPE,LENS_TYPE,i,j))
+            params_sl_err[i,j] = 2*np.loadtxt(OUTDIR+'/%sx%s_FitParamsErr_MagFalse_t%ss%s.dat'%(SOURCE_TYPE,LENS_TYPE,i,j))
+            
+    #thetas, model_m = np.loadtxt(OUTDIR+'/%sx%s_FitModel_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,True),
+    #                             usecols=(0,1), unpack=True)
+
+# ------------------------------------- FUNCTIONS FOR COMPARE MAG ON/OFF ANALYSES -------------------------------------
+
 def Plot_gt_MagOnOff(yaxis_label):
+    # Plot the Mag On/Off best-fit models
     fig = plt.figure(figsize = (12,10)) #figsize = (20,14)
     gs = gridspec.GridSpec(nz_source, nz_lens)
     k=0
@@ -75,10 +98,10 @@ def Plot_gt_MagOnOff(yaxis_label):
     plt.subplots_adjust(wspace=0, hspace=0)
     plt.show()
     return
-Plot_gt_MagOnOff(r'$\theta\gamma_{\rm t}^{\rm model}$')
 
-
-def Plot_Amplitudes():
+def Plot_MagOnOff_Amplitudes():
+    # Compare the amplitudes obtained in the Mag On/Off cases
+    
     plt.figure()
     #plt.scatter( np.arange(20)+1, 1e3*params_m[:20], marker='x', color='red', label='Mag On' )
     #plt.scatter( np.arange(20)+1, 1e3*params_nm[:20], marker='o', color='blue', label='Mag Off' )
@@ -92,8 +115,47 @@ def Plot_Amplitudes():
     plt.legend(loc='best', frameon=True)
     plt.show()
     return
-Plot_Amplitudes()
+
+if Compare_Mag:
+    Plot_gt_MagOnOff(r'$\theta\gamma_{\rm t}^{\rm model}$')
+    Plot_MagOnOff_Amplitudes()
     
+# ------------------------------------- ^^^FUNCTIONS FOR COMPARE MAG ON/OFF ANALYSES^^^ -------------------------------------
 
 
+
+
+
+# ------------------------------------- FUNCTIONS FOR COMPARE SINGLE BIN ANALYSIS -------------------------------------
+def Plot_SingleBin_Params():
+    ylimits = [ [ -2.5, 3. ], [ 0.0, 0.08], [ 0.0, 0.059], [ 0.0, 0.039], [ 0.015, 0.039] ]
+    
+    # Plot the Mag On/Off best-fit models
+    fig = plt.figure(figsize = (8,6)) #figsize = (20,14)
+    gs = gridspec.GridSpec(nz_source, 1)
+    colors=[ 'red', 'blue', 'orange', 'green', 'magenta' ]
+    for i in range(nz_source):
+        ax1 = plt.subplot(gs[i])
+        ax1.fill_between( np.arange(6)+0.5 , (params_sl[i,0]-params_sl_err[i,0]),
+                          (params_sl[i,0]+params_sl_err[i,0]), color='dimgrey', alpha=0.5 )
+        ax1.errorbar( range(1,nz_lens+1), params_sl[i,:], yerr=params_sl_err[i,:], fmt='o',
+                      color=colors[i], edgecolor=None )
+        if i==(nz_source-1):
+            ax1.set_xlabel('Tomo bin number')
+            #ax1.set_xticks([])
+        else:
+            ax1.set_xticks([])
+
+        if i==2:
+            ax1.set_ylabel('Amplitude')
         
+        ax1.set_ylim([ ylimits[i][0] , ylimits[i][1] ])
+        #ax1.set_ylim([ 0.5*(params_sl[i,0]-params_sl_err[i,0]).min(),
+        #               2.0*(params_sl[i,0]+params_sl_err[i,0]).max() ])
+        ax1.text(0.95,0.95, 'sp%s'%(i+1), ha="right", va="top", transform=plt.gca().transAxes)
+        
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.show()
+    return
+Plot_SingleBin_Params()
+# ------------------------------------- ^^^FUNCTIONS FOR COMPARE SINGLE BIN ANALYSIS^^^ ------------------------------------- 
--- Shear_Ratio_Test/GGL_shear_ratio_test_zall.py
+++ Shear_Ratio_Test/GGL_shear_ratio_test_zall.py
@@ -1,14 +1,16 @@
 #Script from Hendrik - hacked by Catherine
 # Hacked again by B. Giblin in the winter of 2019/20 and built
 # better, faster, stronger. We have the technology.
-# (It now works on MICE mocks, K1000/BOSS/GAMA data,
-# and the gamma_t model includes a new parameter to
-# fit for the effects of magnification).
 
-#Now we use treecorr and Mandelbaum estimator
-#to do the random and boost correction
-#we can simplify this script
-#I also removed the scale selection part
+# The code now works on MICE mocks, K1000/BOSS/GAMA data,
+# and the gamma_t model includes a new parameter to
+# fit for the effects of magnification.
+# Also you can toggle Single_Bin True/False to perform
+# SRT for a single lens+source bin (specified on the command line).
+
+# Now we use treecorr and Mandelbaum estimator
+# to do the random and boost correction
+# I also removed the scale selection part
 # and we just use BOSS, so it's easier than
 # the previous 2 spec-catalogue
 
@@ -28,8 +30,10 @@ plt.rc('font', size=10)
 # ----- Load Input ----- #
 DFLAG = ''                    # Data Flag. Set to '' for Fiducial MICE. '_Octant' for MICE Octant.
 Include_mCov = True           # Include the uncertainty due to the m-correction
-Include_Hartlap = False       # Hartlap correction
-Include_Magnification = True  # If True, include extra param in gamma_t model: strength of magnifcation effect on gt.
+Include_Hartlap = False        # Hartlap correction
+Include_Magnification = False   # If True, include extra param in gamma_t model: strength of magnifcation effect on gt.
+Single_Bin = False             # If True, fit for only a single lens-source bin, specified by user on the command line.
+                               # Else fit for all bins simultaneously.
 
 from Get_Input import Get_Input
 paramfile = sys.argv[1]   # e.g. params_KiDSdata.dat
@@ -38,6 +42,27 @@ SOURCE_TYPE = GI.Source_Type()
 LENS_TYPE = GI.Lens_Type()
 RANDOM_TYPE = GI.Random_Type()
 
+if Single_Bin == True:
+    print("Single_Bin is True, so fitting one amplitude parameter to the specified lens and source bin.")
+    print("These must be specified, lens-bin(0-4), source-bin(0-4), on the command line.")
+    spec_bins = [ int(sys.argv[2]) ] # spec bin number (0-4)
+    tomo_bins = [ int(sys.argv[3]) ] # tomo bin number (0-4)
+    ntomo=1                     # Number of photo-z bins
+    nspecz=1                    # "..." of spec-z bins
+
+    # Note the tomo & source bin in the output save-file
+    save_tag = '_t%ss%s' %(tomo_bins[0],spec_bins[0])
+
+else:
+    print("Performing SRT for all lens and source bins simultaneously. ",
+          "Will fit an amplitude parameter per lens and source bin.")
+    ntomo=5              # Number of photo-z bins
+    nspecz=5             # "..." of spec-z bins
+    spec_bins=range(nspecz)
+    tomo_bins=range(ntomo)
+    save_tag = ''        # Blank save_tag means SRT was conducted on all lens & source bins
+    
+numz_tag=5               # Part of 'GT_' filenames; GT_6Z_source_Y_${numz_tag}Z_lens_X.asc
 ntheta = 4
 theta_min = 2.
 theta_max = 30.
@@ -75,8 +100,6 @@ if "MICE2" in SOURCE_TYPE:
     DlsDIR += '_Pz%s_mag%s%s' %(Pz,Mag_OnOff,DFLAG)
 
 
-ntomo=5              # Number of photo-z bins
-nspecz=5             # "..." of spec-z bins
 nspin=500
 if Cov_Method == "Spin":
     ncycle = nspin
@@ -90,10 +113,10 @@ else:
     print("Cov_Method must be set to Spin or Patch. Currently it's set to %s. EXITING." %Cov_Method)
     sys.exit()
 
-for tomobin in range(ntomo):
-    for speczbin in range(nspecz):
-
-        gtfile='%s/GT_6Z_source_%s_%sZ_lens_%s%s.asc' %(INDIR,(tomobin+1), nspecz,(speczbin+1), DFLAG)
+for tomobin in tomo_bins:
+    for speczbin in spec_bins:
+        print("Reading tomo bin %s, spec bin %s" %(tomobin+1, speczbin+1))
+        gtfile='%s/GT_6Z_source_%s_%sZ_lens_%s%s.asc' %(INDIR,(tomobin+1), numz_tag,(speczbin+1), DFLAG)
         gtdat=ascii.read(gtfile)
 
         measurements['thetas_'+str(speczbin)+"_"+str(tomobin)] = gtdat['meanr']
@@ -110,7 +133,7 @@ for tomobin in range(ntomo):
         speczbin_list.append(speczbin*np.ones((ntheta),dtype=np.int16))
 
         # Read in the Dls_over_Ds data created with Dls_over_Ds.py
-        Dls_over_Ds_file = '%s/Dls_over_Ds_DIR_6Z_source_%s_%sZ_lens_%s.asc' %(DlsDIR, (tomobin+1), nspecz,(speczbin+1))
+        Dls_over_Ds_file = '%s/Dls_over_Ds_DIR_6Z_source_%s_%sZ_lens_%s.asc' %(DlsDIR, (tomobin+1), numz_tag,(speczbin+1))
         Dls_over_Ds_tmp = np.loadtxt(Dls_over_Ds_file)
         measurements['Dls_over_Ds_'+str(speczbin)+"_"+str(tomobin)] = np.repeat(Dls_over_Ds_tmp,ntheta)
         Dls_over_Ds_list.append(measurements['Dls_over_Ds_'+str(speczbin)+"_"+str(tomobin)])
@@ -128,26 +151,31 @@ gtlens=np.zeros([ntheta,ncycle])
 diag=np.zeros(nmatrix)
 covdiag=np.zeros([nmatrix,nmatrix])
 
-for tomobin in range(ntomo):
-    for speczbin in range(nspecz):
+for tomobin in tomo_bins:
+    for speczbin in spec_bins:
         for icycle in range(ncycle):
             if Cov_Method == "Spin":
                 gtfile='%s/SPIN/GT_SPIN_%s_6Z_source_%s_%sZ_lens_%s.asc' %(INDIR,icycle,
-                                                                           tomobin+1,nspecz,speczbin+1)
+                                                                           tomobin+1,numz_tag,speczbin+1)
             elif Cov_Method == "Patch":
                 gtfile='%s/PATCH/GT_PATCH_%sof%s_6Z_source_%s_%sZ_lens_%s.asc' %(INDIR,icycle,nPatch*nPatch,
-                                                                                 tomobin+1,nspecz,speczbin+1)
+                                                                                 tomobin+1,num_ztag,speczbin+1)
                 
             gtcycledat=ascii.read(gtfile)
             gtlens[:,icycle]=gtcycledat['gamT'] 
-        if speczbin==0 and tomobin==0:      
-            gtprev = np.copy(gtlens)
+        if speczbin==spec_bins[0] and tomobin==tomo_bins[0]:
+            if Single_Bin:
+                # Then we're only reading in one bin,
+                gtall = np.copy(gtlens)
+            else:
+                # There's more bins to read in
+                gtprev = np.copy(gtlens)
         else:    
             gtall = np.vstack((gtprev,gtlens))
             gtprev= np.copy(gtall)
             print(len(gtall))
 
-cov_gt=np.cov(gtall) 
+cov_gt=np.cov(gtall)
 if Include_mCov:
     # Here we include the uncertainty due to the m-correction
     sigma_m = [0.019, 0.020, 0.017, 0.012, 0.010] # m-uncert. per source bin.
@@ -185,7 +213,7 @@ plt.imshow(Corr, interpolation='None')
 plt.colorbar()
 plt.axis('off')
 #plt.show()
-plt.savefig(OUTDIR+'/%sx%s_Shear_ratio_correlation_matrix.png'%(SOURCE_TYPE,LENS_TYPE))
+plt.savefig(OUTDIR+'/%sx%s_Shear_ratio_correlation_matrix%s.png'%(SOURCE_TYPE,LENS_TYPE,save_tag))
 
 #cov_inv=np.linalg.inv(cov)
 # Catherine reckons these two lines are a more stable way of inverting
@@ -202,14 +230,17 @@ if Include_Hartlap:
 #############################
 
 def func(params):
-    # There is ntheta*nspecz amplitude values in 'params'
-    # and if Magnif is included, additonally nspecz alpha values.
-    amplitude = params[:(ntheta*nspecz)]
+    amplitude = params[:(num_amplitudes)]
     amplitude = np.hstack((amplitude,)*ntomo) # repeat array ntomo times
     model = Dls_over_Ds * amplitude
-
+    if Single_Bin:
+        # If fitting only a single s&l bin, the amplitudes don't change with theta,
+        # so we need to manually include the 1/theta dependence in the model.
+        model *= (1./thetas)
+    
     if Include_Magnification:
-        alpha = params[(ntheta*nspecz):]
+        # If including magnification there's an additional nspecz alpha free params.
+        alpha = params[(num_amplitudes):]
         alpha = np.repeat(alpha, ntheta)  # repeat each element ntheta times (no. points per lens bin)
         alpha = np.hstack((alpha,)*ntomo) # then create ntomo copies of this array
         model = model + 2.*(alpha-1.) * Magnif_Shape
@@ -219,51 +250,70 @@ def func(params):
 
 def chi2(params, data, cov_inv):
     if Include_Magnification:
-        #params[(ntheta*nspecz):] = np.ones(5) # Manually kill the magnification.
+        #params[num_amplitudes:] = np.ones(5) # Manually kill the magnification.
         
         # Apply a prior on the alpha parameters
-        if params[(ntheta*nspecz):].min()<0. or params[(ntheta*nspecz):].max()>5.:
+        if params[num_amplitudes:].min()<0. or params[num_amplitudes:].max()>5.:
         #    print("min params is ", params[(ntheta*nspecz):].min(),
         #          " max params is ", params[(ntheta*nspecz):].max(),
         #          " returning chi2 of infinity.")
             return np.inf
-        
-
     return np.dot(data-func(params),np.dot(cov_inv,data-func(params)))
 
+
+# The gamma_t model depends on if we are running the SRT with all source & lens bins together
+# Or just one source+lens bin.                                                                                                  
+if Single_Bin:
+    # There is only one free amplitude parameter.
+    num_amplitudes = 1
+else:
+    # There are ntheta*nspecz amplitude free params                                                                             
+    # (one per lens and theta bin).                                                                                             
+    num_amplitudes = ntheta*nspecz
+    
 if Include_Magnification:
     # Read in the model for magnifcation shape, given the K1000/BOSS nofz's
     # (NB: this shape won't be a good fit for other nofz's)
     # There's also nspecz more free params in the model (one alpha per lens bin)
     Magnif_Shape = np.load('/home/bengib/kcap_NewInst/kcap/examples/output_magnification_alpha1.0/SRTparam_Bij.npy').flatten()
-    nfreeparams = ntheta*nspecz + nspecz
+    
+    if Single_Bin:
+        # Extract just the elements of Magnif_Shape corresponding to the bins specified by the user
+        idx_mag = tomo_bins[0]*ntheta*5 + spec_bins[0]*ntheta # 1st element in the specified bin
+        Magnif_Shape = Magnif_Shape[idx_mag:(idx_mag+ntheta)]
+
+    nfreeparams = num_amplitudes + nspecz
     params_initial = np.zeros(nfreeparams) 
     
-    # Optimisation might be sensitive to initial values. Set initial alphas to no-mag case.
-    params_initial[ntheta*nspecz:] = np.ones(nspecz) 
+    # Optimisation is VERY sensitive to initial values.
+    # Set initial alphas to no-mag case (this initialisation seems to achieve best chi^2).
+    params_initial[num_amplitudes:] = np.ones(nspecz) 
 else:
-    # If not accounting for magnification, just fitting one amplitude parameter
-    # per lens and theta bin.
-    nfreeparams = ntheta*nspecz
+    # If not accounting for magnification, just fitting for the amplitude parameter(s)
+    nfreeparams = num_amplitudes
     params_initial = np.zeros(nfreeparams)
 
 
-
 ndof = (ntomo * nspecz * ntheta) - nfreeparams
-result = minimize(chi2, params_initial, args=(gt, cov_inv), options={'maxiter': 200000})#, method='TNC' )
+result = minimize(chi2, params_initial, args=(gt, cov_inv), options={'maxiter': 200000}, method='BFGS')
     
 chi2_red = result['fun']/ndof
 p_value = 1 - stats.chi2.cdf(result['fun'], ndof)
 
 print("chi^2=%1.2f, dof=%i, chi^2/dof=%1.2f, p=%1.3f, success=%s\n" % (result['fun'], ndof, chi2_red, p_value, result['success']))
 
-f = open(OUTDIR+'/%sx%s_Shear_ratio.res'%(SOURCE_TYPE,LENS_TYPE), 'w')
+f = open(OUTDIR+'/%sx%s_Shear_ratio%s.res'%(SOURCE_TYPE,LENS_TYPE,save_tag), 'w')
 f.write("chi^2=%1.2f, dof=%i, chi^2/dof=%1.2f, p=%1.3f, success=%s\n" % (result['fun'], ndof, chi2_red, p_value, result['success']))
 f.close()
 
 params_0=result['x']
-np.savetxt(OUTDIR+'/%sx%s_FitParams_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,Include_Magnification), params_0)
-np.savetxt(OUTDIR+'/%sx%s_FitModel_Mag%s.dat'%(SOURCE_TYPE,LENS_TYPE,Include_Magnification),
+# Based on the following stackoverflow page, it looks like
+# the sqrt of Hessian matrx diag is the error on the fitted parameters
+# https://stackoverflow.com/questions/43593592/errors-to-fit-parameters-of-scipy-optimize
+params_0err=np.sqrt( np.diag(result['hess_inv']) )
+np.savetxt(OUTDIR+'/%sx%s_FitParams_Mag%s%s.dat'%(SOURCE_TYPE,LENS_TYPE,Include_Magnification,save_tag), params_0)
+np.savetxt(OUTDIR+'/%sx%s_FitParamsErr_Mag%s%s.dat'%(SOURCE_TYPE,LENS_TYPE,Include_Magnification,save_tag), params_0err)
+np.savetxt(OUTDIR+'/%sx%s_FitModel_Mag%s%s.dat'%(SOURCE_TYPE,LENS_TYPE,Include_Magnification, save_tag),
            np.transpose(np.vstack(( thetas,func(params_0) )) ) )
 
 ######### plots ###
@@ -304,21 +354,29 @@ for i in range(ntomo):
         #gterr_to_plot = gterr[minplt:maxplt]
         #to do the Dls/Ds functions!!!
         func_to_plot = func(params_0)[minplt:maxplt]
+        if Single_Bin:
+            # In this case, axs is not subscriptable (it's 1x1 in size)
+            tmp_axs = axs      # avoids annoying plotting error
+        else:
+            tmp_axs = axs[i,j]
+        tmp_axs.set_xscale('log')
+        tmp_axs.set_xlim(theta_min*0.99,theta_max*2.)
+        tmp_axs.set_ylim(-0.0075,0.025)
 
-        axs[i,j].set_xscale('log')
-        axs[i,j].set_xlim(theta_min*0.99,theta_max*2.)
-        axs[i,j].set_ylim(-0.0075,0.025)
+        tmp_axs.errorbar(thetas_to_plot, thetas_to_plot*gt_to_plot, yerr=thetas_to_plot*gterr_to_plot, fmt='o', capsize=2, markersize=4.)
+        tmp_axs.plot(thetas_to_plot, thetas_to_plot*func_to_plot)
 
-        axs[i,j].errorbar(thetas_to_plot, thetas_to_plot*gt_to_plot, yerr=thetas_to_plot*gterr_to_plot, fmt='o', capsize=2, markersize=4.)
-        axs[i,j].plot(thetas_to_plot, thetas_to_plot*func_to_plot)
+        tmp_axs.text(theta_min*2., 0.013, "t "+str(tomo_bins[i]+1)+", sp "+str(spec_bins[j]+1))
+        tmp_axs.axhline(0., color='gray', linestyle=':', linewidth=1.)
 
-        axs[i,j].text(theta_min*2., 0.013, "t "+str(i+1)+", sp "+str(j+1))
-        axs[i,j].axhline(0., color='gray', linestyle=':', linewidth=1.)
-
-
-axs[2,0].set_ylabel(r"$\theta \times <\gamma_t>$ [arcmin]")
-for i in range(nspecz):
-    axs[-1,i].set_xlabel(r"$\theta$ [arcmin]")
-fig.suptitle(str(LENS_TYPE)+ " lenses, " + str(SOURCE_TYPE) +" sources,  $\chi^2/$dof={:1.2f}".format(chi2_red)+",  $p$={:2.1f}%".format(p_value*100.))
-plt.savefig(OUTDIR+'/%sx%s_Shear_ratio%s.png'%(SOURCE_TYPE,LENS_TYPE,DFLAG))
+if Single_Bin:
+    axs.set_ylabel(r"$\theta \times <\gamma_t>$ [arcmin]")
+    axs.set_xlabel(r"$\theta$ [arcmin]")
+else:
+    axs[2,0].set_ylabel(r"$\theta \times <\gamma_t>$ [arcmin]")
+    for i in range(nspecz):
+        axs[-1,i].set_xlabel(r"$\theta$ [arcmin]")
+        
+fig.suptitle(str(LENS_TYPE)+ " lenses, " + str(SOURCE_TYPE) +" sources,  $\chi^2/$dof={:1.2f}".format(chi2_red)+",  $p$={:2.1f}%".format(p_value*100.) + save_tag)
+plt.savefig(OUTDIR+'/%sx%s_Shear_ratio%s%s.png'%(SOURCE_TYPE,LENS_TYPE,DFLAG,save_tag))
 plt.show()
