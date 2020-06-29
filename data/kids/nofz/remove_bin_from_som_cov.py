25132cdbee866de1f7824f971bc635cc9c1eaea4
Marika Asgari <ma@roe.ac.uk>
2020-06-29 17:41:40 +0100

    som_cov removed bin4 added

---
+++ data/kids/nofz/remove_bin_from_som_cov.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+file=open('SOM_cov_multiplied.asc')
+cov_in=np.loadtxt(file)
+
+L_in = np.linalg.cholesky(cov_in) 
+inv_L_in = np.linalg.inv(L_in)
+
+bins_to_remove=[4]
+
+# remove row and column from cov
+cov = cov_in.copy()
+str_bin_names=''
+for bin_i in bins_to_remove:
+	cov[bin_i-1,:]=0.
+	cov[:,bin_i-1]=0.
+	cov[bin_i-1,bin_i-1]=1.
+	str_bin_names+=str(bin_i)
+	str_bin_names+='_'
+
+
+filename='SOM_cov_multiplied_bin'+str_bin_names+'removed.asc'
+np.savetxt(filename,cov,fmt='%.4e')
+
+
+L = np.linalg.cholesky(cov) 
+
+filename='deltaz.asc'
+file=open(filename)
+delta_z_in=np.loadtxt(file,comments='#')
+
+delta_x_in = np.dot(inv_L_in,delta_z_in)
+
+inv_L = np.linalg.inv(L)
+
+delta_x = np.dot(inv_L,delta_z_in)
+
+delta_z = np.dot(L,delta_x)
+
+print('bins_to_remove:',bins_to_remove)
+# print('delta_z_in=',delta_z_in)
+print('delta_x=',delta_x)
+# print('delta_z=',delta_z)
+
+nBins=5
+for z in range(1,nBins+1):
+	if z in bins_to_remove:
+		print('removed bin:',z)
+	else:
+		print('delta_x_in-delta_x=','%0.3f' %  (delta_x_in-delta_x)[z-1])
+
+
