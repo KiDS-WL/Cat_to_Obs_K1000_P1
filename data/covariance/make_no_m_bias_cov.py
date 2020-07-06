5c869af59a07becb7b30770ee1028e2453a99753
Marika Asgari <ma@roe.ac.uk>
2020-07-06 11:13:43 +0100

    no m bias fits files

---
+++ data/covariance/make_no_m_bias_cov.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pylab as pl
+
+
+
+blind = 'C'
+filename='outputs/Covariance_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5.ascii'
+file=open(filename)
+cov_all=np.loadtxt(file,comments='#')
+
+filename='outputs/Covariance_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5_sigma_m_from_m_cov_0.0000.ascii'
+file=open(filename)
+cov_sigma_m=np.loadtxt(file,comments='#')
+
+cov_no_m_bias = cov_all - cov_sigma_m
+
+filename='outputs/Covariance_no_m_bias_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5.ascii'
+np.savetxt(filename,cov_no_m_bias,fmt='%.20e')
+
+
+# filename='outputs/Covariance_no_m_bias_blindA_nMaximum_20_0.50_300.00_nBins5.ascii'
+# file=open(filename)
+# cov_no_m_bias_a = np.loadtxt(file,comments='#')
+
+# pl.imshow((cov_no_m_bias/cov_no_m_bias_a)-1)
+# pl.colorbar()
+# pl.show()
