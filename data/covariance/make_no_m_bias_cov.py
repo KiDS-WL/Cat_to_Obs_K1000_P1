62ca162f29b48540f94c33d3619880951e6d9bdd
Marika Asgari <ma@roe.ac.uk>
2020-07-08 12:26:54 +0100

    iterative covariance results for COSEBIs

--- data/covariance/make_no_m_bias_cov.py
+++ data/covariance/make_no_m_bias_cov.py
@@ -4,17 +4,17 @@ import matplotlib.pylab as pl
 
 
 blind = 'C'
-filename='outputs/Covariance_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5.ascii'
+filename='outputs/Covariance_bestfit_3x2pt_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5.ascii'
 file=open(filename)
 cov_all=np.loadtxt(file,comments='#')
 
-filename='outputs/Covariance_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5_sigma_m_from_m_cov_0.0000.ascii'
+filename='outputs/Covariance_bestfit_3x2pt_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5_sigma_m_from_m_cov_0.0000.ascii'
 file=open(filename)
 cov_sigma_m=np.loadtxt(file,comments='#')
 
 cov_no_m_bias = cov_all - cov_sigma_m
 
-filename='outputs/Covariance_no_m_bias_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5.ascii'
+filename='outputs/Covariance_bestfit_3x2pt_no_m_bias_blind'+blind+'_nMaximum_20_0.50_300.00_nBins5.ascii'
 np.savetxt(filename,cov_no_m_bias,fmt='%.20e')
 
 
--- data/kids/nofz/cc_cov_and_deltaz.py
+++ data/kids/nofz/cc_cov_and_deltaz.py
@@ -33,11 +33,11 @@ delta_x = np.dot(inv_L,delta_z)
 print('CC+SOM:',delta_x)
 
 
-# import scipy.linalg as la
-# import matplotlib.pylab as plt
-# filename='SOM_cov_multiplied.asc'
-# file=open(filename)
-# cov_som=np.loadtxt(file,comments='#')
+import scipy.linalg as la
+import matplotlib.pylab as plt
+filename='SOM_cov_multiplied.asc'
+file=open(filename)
+cov_som=np.loadtxt(file,comments='#')
 
 # lambda_som, v = la.eig(cov_som)
 # lambda_cc, v = la.eig(cov)
@@ -45,11 +45,32 @@ print('CC+SOM:',delta_x)
 # plt.plot(np.sort(np.real(lambda_som)),'-r',marker='o')
 # plt.plot(np.sort(np.real(lambda_cc)),'-b',marker='s')
 # plt.show()
+nBins=5
+corr = cov.copy()
+for i in range(nBins):
+	for j in range(nBins):
+		corr[i,j]= cov[i,j]/np.sqrt(cov[i,i]*cov[j,j])
+
+nBins=5
+corr_som = cov_som.copy()
+for i in range(nBins):
+	for j in range(nBins):
+		corr_som[i,j]= cov_som[i,j]/np.sqrt(cov_som[i,i]*cov_som[j,j])
+
+vmin=min(corr.min(),corr_som.min()) 
+vmax=max(corr.max(),corr_som.max())
+
+fig, axes = plt.subplots(nrows=1, ncols=2)
+im=axes[0].imshow(corr,vmin=vmin,vmax=vmax,aspect='auto')
+axes[1].imshow(corr_som,vmin=vmin,vmax=vmax,aspect='auto')
+plt.colorbar(im)
+plt.show()
 
 # plt.subplot(1,2,1)
-# plt.imshow(cov_som)
+# plt.imshow(corr)
 # plt.subplot(1,2,2)
-# plt.imshow(cov)
+# plt.imshow(corr_som)
+# plt.colorbar()
 # plt.show()
 
 
